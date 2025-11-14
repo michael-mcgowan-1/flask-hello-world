c410da9777eb4991fd2a7ef631a50a34c8e94608
LowFatHoneyBuns <50250755+LowFatHoneyBuns@users.noreply.github.com>
2025-11-14 01:54:26 +0000

    basketball table creation

--- app.py
+++ app.py
@@ -9,7 +9,24 @@ def hello_world():
 
 
 @app.route('/db_test/')
-def db_test():
+def testing():
     conn = psycopg2.connect("postgresql://mimc5172_flask_hello_database_user:ysYGiRVYygPQ2C7szDG6d67p7jHq2PbY@dpg-d4b8kendiees73adp6j0-a/mimc5172_flask_hello_database")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create/')
+def creating():
+    conn = psycopg2.connect("postgresql://mimc5172_flask_hello_database_user:ysYGiRVYygPQ2C7szDG6d67p7jHq2PbY@dpg-d4b8kendiees73adp6j0-a/mimc5172_flask_hello_database")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
