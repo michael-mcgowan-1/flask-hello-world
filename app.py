1727ea220d9ff624cd72340cb4451efb0989fa88
LowFatHoneyBuns <50250755+LowFatHoneyBuns@users.noreply.github.com>
2025-11-14 01:50:47 +0000

    index change and db connection

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World from Michael McGowan in 3308'
+
+
+@app.route('/db_test/')
+def db_test():
+    conn = psycopg2.connect("postgresql://mimc5172_flask_hello_database_user:ysYGiRVYygPQ2C7szDG6d67p7jHq2PbY@dpg-d4b8kendiees73adp6j0-a/mimc5172_flask_hello_database")
+    conn.close()
+    return "Database Connection Successful"
